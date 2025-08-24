4a24406033e5ee8fee59a509e6baf242a3563244
Aman Manna <aman.manna@planetspark.in>
2025-08-24 21:16:37 +0530

    Companion form created

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CompanionForm from '@/components/CompanionForm'
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main className='min-lg:w-1/3 min-md:w-2/3 items-center justify-center'>
+        <article className='flex flex-col gap-4'>
+            <h1>Companion Builder</h1>
+            <CompanionForm />
+        </article>
+    </main>
   )
 }
 
